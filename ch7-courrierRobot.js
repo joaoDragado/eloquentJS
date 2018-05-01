4250344a6d1d516f6be1df10245b496e6498496e
joaoDragado <dubmecoco@yahoo.com>
2018-05-01 21:28:38 +0300

    setRoute & BFS Courrier built.

--- ch7-courrierRobot.js
+++ ch7-courrierRobot.js
@@ -33,7 +33,7 @@ const roadGraph = buildGraph(roads);
 //console.log(roadGraph);
 
 class Itinerary {
-  // courrier has its current location & its undelivered parcels
+  // courier has its current location & its undelivered parcels
   constructor(location, parcels) {
     this.location = location;
     this.parcels = parcels;
@@ -57,20 +57,20 @@ class Itinerary {
 
 /*
 main function ; turn-based (aka stepwise). 
-args :the courrier, 
+args :the courier, 
       the itinerary (present location + parcels),
       memory of path covered.
 Each step updates the itinerary/memory, & moves to new path point(location).      
 function exits when there are 0 parcels for delivery.
 */
-function courrierRun(itinerary, courrier, memory) {
+function courierRun(itinerary, courier, memory) {
   for (let turn = 0;; turn++) {
     if (itinerary.parcels.length == 0) {
       /*console.log(`Done in ${turn} turns`);
       break;*/
       return turn;
   }
-  let step = courrier(itinerary, memory);
+  let step = courier(itinerary, memory);
   itinerary = itinerary.move(step.newLocation);
   memory = step.memory;
   //console.log(`Moved to ${step.newLocation}`);
@@ -103,15 +103,15 @@ Itinerary.random = function(parcelCount = 5) {
 };
 
 // Test Run
-//courrierRun(Itinerary.random(), randomCourier);
-//console.log(courrierRun(Itinerary.random(), randomCourier));
+//courierRun(Itinerary.random(), randomCourier);
+//console.log(courierRun(Itinerary.random(), randomCourier));
 
 /*
 // frequency analysis of 1000 simulation runs
 let duration = 1000; 
 let runs = [];
 for (var i = 0; i < duration; i++) {
-  runs.push(courrierRun(Itinerary.random(), randomCourier));
+  runs.push(courierRun(Itinerary.random(), randomCourier));
 }
 
 let simMean = Math.floor(runs.reduce((acc, curr) => acc + curr) / runs.length);
@@ -119,4 +119,99 @@ console.log(`No of Simulations : ${runs.length}
 Mean Simulation Run : ${simMean}`);
 // -> No of Simulations : 1000
 // -> Mean Simulation Run : 69
+*/
+
+/*
+----------
+Using a predefined route that reaches all nodes , and input it in the courier memory.
+Worst-case should be 2*length-of-graph.
+*/
+
+const mailRoute = [
+"Alice's House", "Cabin", "Alice's House", "Bob's House",
+"Town Hall", "Daria's House", "Ernie's House",
+"Grete's House", "Shop", "Grete's House", "Farm",
+"Marketplace", "Post Office"
+];
+
+function setCourier(state, memory) {
+if (!memory || memory.length == 0) {
+memory = mailRoute;
+}
+return {newLocation: memory[0], memory: memory.slice(1)};
+}
+/*
+// Test Run
+//courierRun(Itinerary.random(), setCourier);
+//console.log(courierRun(Itinerary.random(), setCourier));
+
+
+// frequency analysis of 1000 simulation runs
+let duration = 1000; 
+let runs = [];
+for (var i = 0; i < duration; i++) {
+  runs.push(courierRun(Itinerary.random(), setCourier));
+}
+
+let simMean = Math.floor(runs.reduce((acc, curr) => acc + curr) / runs.length);
+console.log(`No of Simulations : ${runs.length}
+Mean Simulation Run : ${simMean}`);
+// -> No of Simulations : 1000
+// -> Mean Simulation Run : 18
+*/
+
+/*
+-------
+courier is finding the shortest route in a connected graph, without weights or direction.
+Alternative would have been BFS, A* algorithms. (both recursive)
+*/
+function shortestRoute(graph, start, dest) {
+  let work = [{current: start, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {current, route} = work[i];
+    for (let node of graph[current]) {
+      if (node == dest) return route.concat(node);
+      if (!work.some(w => w.current == node)) {
+        work.push({current: node, route: route.concat(node)});
+      }
+    }
+  }
+}
+/*
+//console.log(roadGraph);
+
+console.log(shortestRoute(roadGraph, "Post Office", "Farm"));
+// -> [ 'Marketplace', 'Farm' ]
+*/
+
+function smartCourier({location, parcels}, route) {
+  if (!route ||route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.location != location) {
+      route = shortestRoute(roadGraph, location, parcel.location);
+    } else {
+      route = shortestRoute(roadGraph, location, parcel.address);
+    }
+  }
+  return {newLocation: route[0], memory: route.slice(1)};
+}
+
+/*
+// Test Run
+//courierRun(Itinerary.random(), smartCourier);
+//console.log(courierRun(Itinerary.random(), smartCourier));
+
+
+// frequency analysis of 1000 simulation runs
+let duration = 1000; 
+let runs = [];
+for (var i = 0; i < duration; i++) {
+  runs.push(courierRun(Itinerary.random(), smartCourier));
+}
+
+let simMean = Math.floor(runs.reduce((acc, curr) => acc + curr) / runs.length);
+console.log(`No of Simulations : ${runs.length}
+Mean Simulation Run : ${simMean}`);
+// -> No of Simulations : 1000
+// -> Mean Simulation Run : 14
 */
